2543ea15a94fdf1ea35938d8b91c2a4db38a1480
arinko_ya <shiroper.yasaku@gmail.com>
2018-09-14 10:28:54 +0900

    Create order counter. #3

--- app/tests/order.py
+++ app/tests/order.py
@@ -1,6 +1,12 @@
 import unittest
+from datetime import datetime
 
 from app import db
+from app.genre.models import Genre
+from app.item.models import Item
+from app.order.models import Order
+from app.order import registration
+from app.utils.log_util import Status
 
 
 class TestOrder(unittest.TestCase):
@@ -8,6 +14,68 @@ class TestOrder(unittest.TestCase):
         db.drop_all()
         db.create_all()
 
+        Genre.add_genre('test_genre')
+        self.genre = Genre.query.filter_by(name='test_genre').first()
+        add_item = {
+            'name': 'test_item',
+            'genre': self.genre,
+            'price': 100,
+            'is_active': True,
+            'is_high_priority': True}
+        Item.add_item(**add_item)
+
+        self.item = Item.query.filter_by(name='test_item').first()
+        self.today = datetime.today().date()
+
+        db.session.add(Order(
+            item=self.item, price=self.item.price * 3,
+            quantity=3, sold_at=self.today)
+        )
+        db.session.commit()
+
     def tearDown(self):
         db.drop_all()
 
+    def test_add_order(self):
+        result = registration.save_order([{
+            'item': self.item, 'quantity': 2, 'sold_at': self.today}])
+        self.assertEqual(result.status, Status.SUCCEEDED)
+
+        order = Order.query.all()
+        self.assertEqual(len(order), 2)
+
+    def test_add_multi_order(self):
+        add_item = {
+            'name': 'test_item_2',
+            'genre': self.genre,
+            'price': 100,
+            'is_active': True,
+            'is_high_priority': True}
+        Item.add_item(**add_item)
+        item2 = Item.query.filter_by(name='test_item_2').first()
+
+        result = registration.save_order([
+            {'item': self.item, 'quantity': 2, 'sold_at': self.today},
+            {'item': item2, 'quantity': 2, 'sold_at': self.today}
+        ])
+        self.assertEqual(result.status, Status.SUCCEEDED)
+
+        order = Order.query.all()
+        self.assertEqual(len(order), 3)
+
+    def test_add_no_quantity(self):
+        add_item = {
+            'name': 'test_item_2',
+            'genre': self.genre,
+            'price': 100,
+            'is_active': True,
+            'is_high_priority': True}
+        Item.add_item(**add_item)
+        item2 = Item.query.filter_by(name='test_item_2')
+
+        result = registration.save_order([{
+            'item': item2, 'quantity': 0, 'sold_at': self.today}])
+        self.assertEqual(result.status, Status.FAILED)
+
+        order = Order.query.all()
+        self.assertEqual(len(order), 1)
